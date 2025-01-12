e5b8f1599c50832aa2c6979d2bd3af1ce1bce2fd
imocanu_rba <imocanu@ritchiebros.com>
2025-01-12 21:42:12 +0200

    Ex 4

--- lab-04-entity-hierarchy/src/main/java/edu/jpa/TABLE_PER_HIERARCHY/entity/Person.java
+++ lab-04-entity-hierarchy/src/main/java/edu/jpa/TABLE_PER_HIERARCHY/entity/Person.java
@@ -2,6 +2,9 @@ package edu.jpa.TABLE_PER_HIERARCHY.entity;
 
 import jakarta.persistence.*;
 
+@Entity
+@Inheritance(strategy=InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name="TYPE", discriminatorType=DiscriminatorType.STRING)
 public abstract class Person {
     @Id
     private int id;
